9889f5aad8d541ee42a9d17f20cb216f73a203b8
collins-ruto <collinsruto48@gmail.com>
2022-06-27 08:53:57 +0300

    make it better

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import ReviewsDAO from "./dao/reviewsDAO.js";
 dotenv.config();
 const MongoClient = mongodb.MongoClient;
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 MongoClient.connect(process.env.RESTREVIEWS_DB_URI, {
   maxPoolSize: 50,
